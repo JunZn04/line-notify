e053d4135974732563ac5b25480d6d94c6fd9c0e
JunZn04 <JunZn04@gmail.com>
2024-08-01 17:03:32 +0800

    新增後端

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const line = require('@line/bot-sdk');
 const { text } = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
@@ -13,6 +15,20 @@ const lineClient = new line.Client(lineConfig);
 
 const app = express();
 
+function getBase64Image(filePath) {
+  const bitmap = fs.readFileSync(filePath);
+  return Buffer.from(bitmap).toString('base64');
+}
+
+const imagePath = path.join(__dirname, '.', 'public','images', '1.jpg'); // 替換為你的本地圖片路徑
+const base64Image = getBase64Image(imagePath);
+
+const imageMessage = {
+  type: 'image',
+  originalContentUrl: `https://i.pinimg.com/564x/04/3a/19/043a198abfc4e47ca2859038bcfac77d.jpg`,
+  previewImageUrl: `https://i.pinimg.com/564x/04/3a/19/043a198abfc4e47ca2859038bcfac77d.jpg`
+};
+
 // lineClient.pushMessage("Ued478d9a14b1d998ed0c3aaf425a739c",[
 //     {
 //         "type": "text",
@@ -20,14 +36,11 @@ const app = express();
 //     }
 // ])
 
-// lineClient.pushMessage("Ued478d9a14b1d998ed0c3aaf425a739c",[
-//     {
-//         "type": "image",
-//         "text": "測試發送文字"
-//     }
-// ])
+lineClient.pushMessage("Ued478d9a14b1d998ed0c3aaf425a739c" ,imageMessage)
+
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
+
 app.post('/webhook', line.middleware(lineConfig), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -41,14 +54,13 @@ app.post('/webhook', line.middleware(lineConfig), (req, res) => {
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
   // create an echoing text message
   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
+  // console.log(event)
   return lineClient.replyMessage(event.replyToken, echo);
 }
 
